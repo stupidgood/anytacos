bfa6ca47e0dfa3e1402c116ba5f414c66c697579
Covington Doan <covington@covingtondoan.com>
2017-08-08 17:01:20 -0500

    Add AJAX and GIPHY

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,29 @@
-console.log('you can use ES6 here : )')
+const $ = require('jquery')
+
+function giphy(searchString) {
+  $.ajax({
+    url: 'https://api.giphy.com/v1/gifs/translate?api_key=' + process.env.GIPHY_API_KEY
+ + '&s=' + searchString,
+    success: function(data) {
+      $('body').css('background-image', 'url(' + data.data.images.fixed_height.url + ')');
+    },
+  })
+}
+
+$.ajax({
+  url: 'https://connect.squareup.com/v1/' + process.env.SQUARE_LOCATION_ID
+ + '/inventory',
+  beforeSend: function(xhr) {
+    xhr.setRequestHeader('Authorization', 'Bearer ' + process.env.SQUARE_PERSONAL_ACCESS_TOKEN);
+  },
+  success: function(data) {
+    if (data[0].quantity_on_hand > 0) {
+      giphy('thumbs up');
+    } else {
+      giphy('no');
+    }
+  },
+  error: function() {
+    giphy('error');
+  },
+})
