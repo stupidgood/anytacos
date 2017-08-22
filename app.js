553174c5b904efda1d441bc3ed1a240a4beb6813
Covington Doan <covington@covingtondoan.com>
2017-08-22 18:12:32 -0500

    Fix dotenv, clean up deps, and refactor
    1. Fix dotenv
    2. Remove spike-css-standards and spike-page-id
    3. Refactor layout

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 
 const htmlStandards = require('reshape-standard')
-const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
-const pageId = require('spike-page-id')
 const DefinePlugin = require('webpack').DefinePlugin
 const env = process.env.NODE_ENV
 
@@ -11,19 +9,14 @@ module.exports = {
   devtool: 'source-map',
   ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
     minify: env === 'production'
-  }),
-  postcss: cssStandards({
-    minify: env === 'production',
-    warnForDuplicates: env !== 'production'
   }),
   babel: jsStandards(),
   plugins: [
     new DefinePlugin({
-      SQUARE_PERSONAL_ACCESS_TOKEN: JSON.stringify(process.env.SQUARE_PERSONAL_ACCESS_TOKEN),
+      GIPHY_API_KEY: JSON.stringify(process.env.GIPHY_API_KEY),
       SQUARE_LOCATION_ID: JSON.stringify(process.env.SQUARE_LOCATION_ID),
-      GIPHY_API_KEY: JSON.stringify(process.env.GIPHY_API_KEY)
+      SQUARE_PERSONAL_ACCESS_TOKEN: JSON.stringify(process.env.SQUARE_PERSONAL_ACCESS_TOKEN),
     })
   ]
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-const $ = require('jquery')
+import $ from 'jquery';
 
 function giphy(searchString) {
   $.ajax({
     url: 'https://api.giphy.com/v1/gifs/translate?api_key=' + GIPHY_API_KEY
  + '&s=' + searchString,
     success: function(data) {
-      $('body').css('background-image', 'url(' + data.data.images.fixed_height.url + ')');
+      $('main').css('background-image', 'url(' + data.data.images.fixed_height.url + ')');
     },
   })
 }
@@ -27,4 +27,4 @@ $.ajax({
   error: function() {
     giphy('error');
   },
-})
+});
